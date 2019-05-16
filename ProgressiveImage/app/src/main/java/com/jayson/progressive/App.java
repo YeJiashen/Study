a5b5801a7d8d96a0341770ba727831431b01203a
Ye Jiashen <yejiashenchn@jd.com>
2019-05-16 21:33:09 +0800

    Implement load local image by progressive

---
+++ ProgressiveImage/app/src/main/java/com/jayson/progressive/App.java
@@ -0,0 +1,9 @@
+package com.jayson.progressive;
+
+/**
+ * @author Ye Jiashen
+ * @date 2019/5/16
+ * @description:
+ */
+public class App {
+}
---
+++ ProgressiveImage/app/src/main/java/com/jayson/progressive/activity/NetActivity.java
@@ -0,0 +1,13 @@
+package com.jayson.progressive.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net);
+    }
+}
